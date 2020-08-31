21f6baa6e9648f2e2c46f60792f98ff477a1d839
zhs <haisong@tiplus.cn>
2020-08-31 18:12:38 +0800

    demo-init

---
+++ dubbo-demo/src/test/java/com/zhs/dubbodemo/DubboDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhs.dubbodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
