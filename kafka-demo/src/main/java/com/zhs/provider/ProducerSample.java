1f7f39d74a6839bbc7bf74856a31deb1dcb38330
zhs <haisong@tiplus.cn>
2020-11-09 18:44:21 +0800

    kafka

--- kafka-demo/src/main/java/com/zhs/provider/ProducerSample.java
+++ kafka-demo/src/main/java/com/zhs/provider/ProducerSample.java
@@ -1,9 +1,6 @@
 package com.zhs.provider;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.*;
 
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
@@ -54,6 +51,39 @@ public class ProducerSample {
         for (int i = 0; i < 10; i++) {
             ProducerRecord<String, String> record = new ProducerRecord<String, String>(TOPIC_NAME,"key-"+i,"value-"+i);
             Future<RecordMetadata> send = producer.send(record);
+            RecordMetadata recordMetadata = send.get();
+            System.out.println("partition:"+recordMetadata.partition()+"offset:"+recordMetadata.offset());
+        }
+    }
+
+    /**
+     * 异步回调
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    public static void produceSyncSendCallBack() throws ExecutionException, InterruptedException {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"49.233.189.184:9092");
+        properties.put(ProducerConfig.ACKS_CONFIG,"all");
+        properties.put(ProducerConfig.RETRIES_CONFIG,"0");
+        properties.put(ProducerConfig.BATCH_SIZE_CONFIG,"16384");
+        properties.put(ProducerConfig.LINGER_MS_CONFIG,"1");
+        properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG,"33554432");
+
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(TOPIC_NAME,"key-"+i,"value-"+i);
+            Future<RecordMetadata> send = producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+
+                    System.out.println("partition:"+metadata.partition()+"offset:"+metadata.offset());
+                }
+            });
             RecordMetadata recordMetadata = send.get();
             System.out.println("partition:"+recordMetadata.partition()+"offset:"+recordMetadata.offset());
         }
